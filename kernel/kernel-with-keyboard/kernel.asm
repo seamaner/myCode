a8456ae04a47ac2c877d51d2c7224874d33f874a
seamaner <quhxquhx@gmail.com>
2016-08-18 15:51:29 +0800

    Update kernel.asm

--- kernel/kernel-with-keyboard/kernel.asm
+++ kernel/kernel-with-keyboard/kernel.asm
@@ -1,20 +1,52 @@
+;;kernel.asm
+
 ;nasm directive - 32 bit
 bits 32
 section .text
-     align 4
-     dd 0x1badb002
-     dd 0x00
-     dd - (0x1badb002 + 0x00)
+        ;multiboot spec
+        align 4
+        dd 0x1BADB002            ;magic
+        dd 0x00                  ;flags
+        dd - (0x1BADB002 + 0x00) ;checksum. m+f+c should be zero
 
 global start
-extern kmain
+global read_port
+global write_port
+global load_idt
+global keyboard_handler
+
+extern kmain	        ;kmain is defined in the c file
+extern keyboard_handler_main
 
 start:
-  cli
-  mov esp, stack_space
+  cli 			;block interrupts
+  mov esp, stack_space	;set stack pointer
   call kmain
-  hlt
+  hlt		 	;halt the CPU
 
- section .bss
- resb 8192
- stack_space:
+read_port:
+  mov edx, [esp + 4]
+  in al, dx
+  ret
+
+write_port:
+   mov edx, [esp + 4]
+   mov al, [esp + 4 + 4]
+   out dx, al
+   ret
+
+load_idt:
+   mov edx, [esp + 4]
+   lidt [edx]
+   sti
+   ret
+
+keyboard_handler:
+    call keyboard_handler_main
+    iretd
+
+
+
+section .bss
+resb 8192		;8KB for stack
+stack_space:
