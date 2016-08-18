3914ec0880df02649e9fb5ee8db2a09f94da7b59
seamaner <quhxquhx@gmail.com>
2016-08-18 15:45:09 +0800

    Update kernel.asm

--- kernel/kernel-without-keyboard/kernel.asm
+++ kernel/kernel-without-keyboard/kernel.asm
@@ -10,13 +10,42 @@ section .text
         dd - (0x1BADB002 + 0x00) ;checksum. m+f+c should be zero
 
 global start
+global read_port
+global write_port
+global load_idt
+global keyboard_handler
+
 extern kmain	        ;kmain is defined in the c file
+extern keyboard_handler_main
 
 start:
   cli 			;block interrupts
   mov esp, stack_space	;set stack pointer
   call kmain
   hlt		 	;halt the CPU
+
+read_port:
+  mov edx, [esp + 4]
+  in al, dx
+  ret
+
+write_port:
+   mov edx, [esp + 4]
+   mov al, [esp + 4 + 4]
+   out dx, al
+   ret
+
+load_idt:
+   mov edx, [esp + 4]
+   lidt [edx]
+   sti
+   ret
+
+keyboard_handler:
+    call keyboard_handler_main
+    iretd
+
+
 
 section .bss
 resb 8192		;8KB for stack
